96ba0647ad84433aa926f8d1c86e32e288f7d100
dhairya <joshidhairaya2002@gmail.com>
2023-07-23 23:37:29 +0530

    added pawn promotion

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -19,8 +19,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a0378a6073d4467fc5d95582f0e9d4ab",
-"/": "a0378a6073d4467fc5d95582f0e9d4ab",
+"index.html": "041863f210ee44bb744c7149dc6971d4",
+"/": "041863f210ee44bb744c7149dc6971d4",
 "main.dart.js": "dd1d3e644eb1d62d1b2ec84b6333b7cf",
 "manifest.json": "2aad07eac24e852dcfe695af2ca2f7e7",
 "version.json": "ebe8e1fac52ae8e38f2af9bc55658c7f"
